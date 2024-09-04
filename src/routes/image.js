758c153aec3eea756ab54f1de3cf9bd1649c567e
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-09-04 11:59:22 +0200

    modif auth middleware multer middleware

--- src/routes/image.js
+++ src/routes/image.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const multer = require("../middleware/multer-config");
 const imageController = require("../controllers/image");
-const auth = require('../middleware/auth')
+const auth = require("../middleware/auth");
 
-router.post("/", multer, auth, imageController.createImage);
+router.post("/", auth, multer, imageController.createImage);
 
-router.get("/" , imageController.getAllImages);
+router.get("/", imageController.getAllImages);
 
 router.get("/:id", imageController.getOneImage);
 
-router.put("/:id", multer, auth, imageController.updateImage);
+router.put("/:id", auth, multer, imageController.updateImage);
 
-router.delete("/:id",auth , imageController.deleteImage);
+router.delete("/:id", auth, imageController.deleteImage);
 
 module.exports = router;
